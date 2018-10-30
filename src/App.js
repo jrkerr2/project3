05f24871bd3a8818b61f81c39bfa67c7c0389fe1
jrkerr2 <johnrkerr@hotmail.com>
2018-10-30 12:44:50 -0400

    custom logo and text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import altLogo from './sun.png';
 import './App.css';
 
 class App extends Component {
@@ -7,17 +8,17 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={altLogo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Project 3: Full stack React-based application coming soon to a Bootcamp near you.
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://trello.com/b/bHI6yGNU/coding-bootcamp-project-3"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Details pending...
           </a>
         </header>
       </div>
